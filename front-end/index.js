3eb69ddad3130f0841437809e0cbc301f6ff0c99
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-18 19:45:58 +0200

    added apis

--- front-end/index.js
+++ front-end/index.js
@@ -3,6 +3,24 @@ const dating_pages = {};
 
 dating_pages.baseURL = "http://127.0.0.1:8000/api";
 
+//for dynamic loading
+dating_pages.loadFor = (page) => {
+    eval("dating_pages.load_" + page + "();");
+};
+
+// Console Tracker
+dating_pages.Console = (title, values, oneValue = true) => {
+    console.log("---" + title + "---");
+    if (oneValue) {
+    console.log(values);
+    } else {
+    for (let i = 0; i < values.length; i++) {
+        console.log(values[i]);
+    }
+    }
+    console.log("--/" + title + "---");
+};
+
 // GET API function
 dating_pages.getAPI = async (api_url) => {
         try {
@@ -29,3 +47,4 @@ dating_pages.postAPI = async (
         dating_pages.Console("Error from POST API", error);
         }
     };
+
