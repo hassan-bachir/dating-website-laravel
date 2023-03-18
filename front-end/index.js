12354e29959653cb9d677aec1283411faf1296de
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-18 21:35:39 +0200

    delete created todo tables and models

--- front-end/index.js
+++ front-end/index.js
@@ -48,3 +48,119 @@ dating_pages.postAPI = async (
         }
     };
 
+
+    dating_pages.load_login = async () => {
+    const formCloser = document.querySelectorAll(".close-form p");
+    const signInBtn = document.getElementById("signIn");
+    const signInForm = document.getElementById("signInForm");
+    const signUpBtn = document.getElementById("signUp");
+    const signUpForm = document.getElementById("signUpForm");
+    const signInSubmit = document.getElementById("signInSubmit");
+    const signUpSubmit = document.getElementById("signUpSubmit");
+    signInBtn.addEventListener("click", () => {
+        signInForm.style.display = "flex";
+    });
+    signUpBtn.addEventListener("click", () => {
+        signUpForm.style.display = "flex";
+    });
+    formCloser.forEach((closer) => {
+        closer.addEventListener("click", () => {
+        signInForm.style.display = "none";
+        signUpForm.style.display = "none";
+        });
+    });
+
+    // -- Login Page Input Controller
+    // -- -- Sign In
+    signInSubmit.addEventListener("click", async (event) => {
+        // Authenticate and Authorize
+        // -- Data Section
+        const signInEmail = document.getElementById("signInEmail");
+        const signInPass = document.getElementById("signInPass");
+        const postData = {
+        email: signInEmail.value,
+        password: signInPass.value,
+        };
+
+        // -- API Section
+        const login_url = `${dating_pages.baseURL}/login`;
+        event.preventDefault();
+        const response_login = await dating_pages.postAPI(login_url, postData);
+        dating_pages.Console("Testing login API", response_login);
+        if (response_login) {
+        localStorage.setItem("jwt", response_login.data.authorisation.token);
+        window.open("./landingPage.html", "_self");
+        } else {
+        alert("UNAUTHORIZED");
+        }
+    });
+
+    // -- -- -- Location Saver SignUp
+    const locationDetectorSignUp = document.getElementById(
+        "locationDetectorSignUp"
+    );
+
+    locationDetectorSignUp.addEventListener(
+        "click",
+        () => {
+        const options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0,
+        };
+
+        const success = async (pos) => {
+            let crd = pos.coords;
+            let lat = crd.latitude.toString();
+            let lng = crd.longitude.toString();
+            coordinates[0] = lat;
+            coordinates[1] = lng;
+            console.log(coordinates);
+        };
+
+        function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        }
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+        },
+        { once: true }
+    );
+
+    // -- -- Sign Up
+    signUpSubmit.addEventListener("click", async (event) => {
+        const signUpName = document.getElementById("signUpName");
+        const signUpEmail = document.getElementById("signUpEmail");
+        const dateOfBirth = document.getElementById("dob");
+        const signUpPass = document.getElementById("signUpPass");
+        const locationUpd = `${coordinates[0]},${coordinates[1]}`;
+        const gender = document.getElementById("gender"); // 0 male; 1 female
+        const selectedGender = document.getElementById("selectedGender"); // 0 male; 1 female
+        const interests = "Edit Interests";
+        const postData = {
+        name: signUpName.value,
+        email: signUpEmail.value,
+        image: "./assets/default.jpg",
+        dob: dateOfBirth.value,
+        password: signUpPass.value,
+        location: locationUpd,
+        gender: gender.value,
+        gender_preference: selectedGender.value,
+        interests: interests,
+        };
+
+        console.log(postData);
+
+        // -- API Section
+        event.preventDefault();
+        const signup_url = `${dating_pages.baseURL}/register`;
+        const response_signup = await dating_pages.postAPI(signup_url, postData);
+        dating_pages.Console("Testing login API", response_signup);
+        if (response_signup) {
+        location.reload();
+        } else {
+        alert("Please use a different email");
+        }
+    });
+    };
+
