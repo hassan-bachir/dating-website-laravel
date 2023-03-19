385e75480dcd81514a082c12514290c4c401e5c8
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-19 22:16:33 +0200

    added getUsers aPI

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -16,7 +16,15 @@ class UserController extends Controller
 {
     public function getUsers(){
         
-        $user = Auth::users();
-        $data = bd::table('users')
+        $user = Auth::users();            //hyde enno elli mta3mik block ma bibayyin 3ndak 
+        $data = DB::table('users')->leftJoin('blocks','users.id','=','blocks.user2_id')->get()->where('user2_id', '=', '')->where('id', '!=', $user->id)->where('gender', '=', $user->preference);
+        $blocks = DB::table('blocks')->where('user1_id', '=', $user->id)->get();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $data,
+            'blocks' => count($blocks)
+        ]);
+
     }
 }
