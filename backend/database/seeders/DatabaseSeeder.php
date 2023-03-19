237138b6eef6c385421b28175d62891e3299f028
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-19 18:55:50 +0200

    added users throught seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Database\Seeders;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +16,44 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+    
+        $current_date_time = Carbon::now()->toDateTimeString();
+        DB::table('users')->insert([
+            'name' => 'jihad',
+            'email' => 'jihad@email.com',
+            'mobile' => '81 123 123',
+            'date_of_birth' => '2000-10-10',
+            'password' => Hash::make('test'),
+            'location' => '32.2395695,35.1110406',
+            'gender' => 'male',
+            'preference' => 'female ',
+            
+            'created_at' => $current_date_time
+        ]);
+        DB::table('users')->insert([
+            'name' => 'isma',
+            'email' => 'isma@email.com',
+            'mobile' => '81 234242',
+            'date_of_birth' => '1990-10-10',
+            'password' => Hash::make('test'),
+            'location' => '32.2395695,35.1110406',
+            'gender' => 'male',
+            'preference' => 'female ',
+            
+            'created_at' => $current_date_time
+        ]);
+        DB::table('users')->insert([
+            'name' => 'imad',
+            'email' => 'imad@email.com',
+            'mobile' => '81 354 123',
+            'date_of_birth' => '1980-10-10',
+            'password' => Hash::make('test'),
+            'location' => '32.2395695,35.1110406',
+            'gender' => 'male',
+            'preference' => 'female ',
+            
+            'created_at' => $current_date_time
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
